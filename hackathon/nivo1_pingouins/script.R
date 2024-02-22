b92ea7544f6858db08f5c308e13c803a2735af4f
Vincent Guillemot <vincent.guillemot@pasteur.fr>
2024-02-22 11:40:46 +0100

    Ajout script pour débutant

--- hackathon/nivo1_pingouins/script.R
+++ hackathon/nivo1_pingouins/script.R
@@ -1,7 +1,23 @@
+## Librairies
 library(mlr3)
+library(mlr3extralearners)
+library(mlr3viz)
 
+## Liste des taches prédéfinies
 mlr_tasks
 
 tsk_peng <- tsk("penguins")
-tsk_peng
-tsk_peng$data()
+tsk_peng # -> "multiclass" parce qu'il y a trois groupes
+tsk_peng$data() # -> pour voir les data
+
+## Automatic plot
+autoplot(tsk_peng)
+
+## Splits
+splits <- partition(tsk_peng)
+
+## Train, pour chercher un learner : https://mlr-org.com/learners.html
+lrn_rpart <- lrn("classif.rpart")
+lrn_rpart$train(tsk_peng, splits$train)
+prediction <- lrn_rpart$predict(tsk_peng, splits$test)
+prediction$score(msr("classif.acc"))
