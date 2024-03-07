6d823466b5dac9731db07718e0f72ad1fcc9f431
Vincent Guillemot <vincent.guillemot@pasteur.fr>
2024-03-07 14:26:54 +0100

    UPDATE LGG data with gene symbol

--- data/make_lgg_smaller.R
+++ data/make_lgg_smaller.R
@@ -1,5 +1,9 @@
+## Librairies
+library(biomaRt)
 library(dplyr)
+library(tidyr)
 
+## Chargement des données
 load("data/LGG.RData")
 
 ## Structure des blocs
@@ -17,9 +21,49 @@ cnv <- cnv[, abs(colMeans(cnv)) >= 0.1]
 rnavars <- apply(rna, 2, var)
 rna <- rna[, order(-rnavars)][, 1:5000]
 
+## Annotation des CNVs et RNAs
+mart <- useDataset("hsapiens_gene_ensembl", useMart("ensembl"))
+
+ensembl_rna <- gsub("_[1-z]+$", "", names(rna))
+ensembl_cnv <- gsub("_[1-z]+$", "", names(cnv))
+
+annot_rna <- getBM(
+  filters = "ensembl_gene_id",
+  attributes = c("ensembl_gene_id", "hgnc_symbol"),
+  values = ensembl_rna,
+  mart = mart)
+annot_cnv <- getBM(
+  filters = "ensembl_gene_id",
+  attributes = c("ensembl_gene_id", "hgnc_symbol"),
+  values = ensembl_cnv,
+  mart = mart)
+annot_both <- rbind(annot_rna, annot_cnv) %>% 
+  drop_na() %>%
+  distinct() %>%
+  mutate(
+    rna_col = paste0(ensembl_gene_id, "_rna"),
+    cnv_col = paste0(ensembl_gene_id, "_cnv"),
+  )
+
+selected_variables_rna <- intersect(annot_both$rna_col, names(rna))
+selected_variables_cnv <- intersect(annot_both$cnv_col, names(cnv))
+
+rna <- rna[, selected_variables_rna]
+cnv <- cnv[, selected_variables_cnv]
+
+names(rna) <- with(
+  annot_both,
+  paste0(make.unique(hgnc_symbol[
+    match(selected_variables_rna, rna_col)]), "_rna"))
+names(cnv) <- with(
+  annot_both,
+  paste0(make.unique(hgnc_symbol[
+    match(selected_variables_cnv, cnv_col)]), "_cnv"))
+
+
 ## Création du "plus petit" jeu de données
 lgg <- cbind(
-  dat %>% select(bcr_patient_barcode, time, status, ends_with("_clinical")),
+  dat %>% dplyr::select(bcr_patient_barcode, time, status, ends_with("_clinical")),
   mutation,
   cnv,
   rna,
