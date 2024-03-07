3de294c330631937924ec610a15eef59c0720253
Vincent Guillemot <vincent.guillemot@pasteur.fr>
2024-03-07 15:17:12 +0100

    ADD classification on mutation with PipeOps

--- hackathon/nivo2_lgg_classif/02_classif_on_mutation.R
+++ hackathon/nivo2_lgg_classif/02_classif_on_mutation.R
@@ -7,15 +7,17 @@ theme_set(theme_bw())
 
 ## Data
 load("hackathon/minilgg.RData")
-rna <- lgg[, grep("_rna$", names(lgg))]
+mutation <- lgg[, grep("_mutation$", names(lgg))]
 y <- factor(lgg$histological_type_Oligoastrocytoma_clinical)
-dat_4_classif <- cbind(y = y, rna)
+dat_4_classif <- cbind(y = y, mutation)
 
 ## Tache + learner
 tsk_lgg <- TaskClassif$new("lgg", dat_4_classif, target = "y", positive = "1")
-lrn_lda <- lrn("classif.lda")
+po_lda <- po("removeconstants") %>>% lrn("classif.lda")
+lrn_lda <- as_learner(po_lda)
 
 cv_rep <- rsmp("repeated_cv", repeats = 2, folds = 3)
 lgg_resamp <- resample(tsk_lgg, lrn_lda, cv_rep)
 autoplot(lgg_resamp, measure = msr("classif.acc"))
+
 
