dd54c70e5f96cfef257039753bd23b67b2c8d89b
vlegoff <160844540+vlegoff@users.noreply.github.com>
2024-03-07 09:42:49 +0100

    Add files via upload

--- R/bench_experiment.R
+++ R/bench_experiment.R
@@ -10,6 +10,8 @@ suppressMessages({
     source("R/misc/bench_misc.R")
 })
 
+options(batchtools.progress=FALSE)
+
 parser = ArgumentParser()
 
 parser$add_argument("-r", "--registry", nargs=1,
@@ -19,16 +21,12 @@ parser$add_argument("-b", "--batchconf",
     help="path to batchtools configuration file")
 parser$add_argument("-N", "--Ncpus", default=1,
     help="number of cpu to use [default %(default)s]")
-#parser$add_argument("-m", "--mode", default="i", nargs=1,
-    #help="Batchtools Cluster Function: i for interactive, m for multicore, s
-    #for slurm [default %(default)s]")
-#parser$add_argument("-t", "--template", default="slurm-simple",
-    #help="template to use for slurm cluster of batchtools [default
-    #%(default)s]")
-#parser$add_argument("--hostname", default="inti.cnrgh.fr",
-    #help="hostname of the cluster frontend node [default %(default)s]")
 parser$add_argument("-s", "--seed", default=1234,
     help="seed used to create resampling folds [default %(default)s]")
+parser$add_argument("--resubmit_errors", action="store_true",
+    help="")
+parser$add_argument("--pegasus", action="store_true",
+    help="")
 
 args = parser$parse_args()
 
@@ -77,11 +75,48 @@ design <- benchmark_grid(tasks, learners, cv10)
 
 batchmark(design, store_models=FALSE, reg=reg)
 
-lapply(names(resources),
-    function(l) submitLrn(l,
-        jobs_per_chunk=resources[[l]]$jobs_per_chunk,
-        resources=resources[[l]],
-        not_done=TRUE))
+if(!args$pegasus) {
+    lapply(names(resources),
+        function(l) submitLrn(l,
+            jobs_per_chunk=resources[[l]]$jobs_per_chunk,
+            resources=resources[[l]],
+            not_done=TRUE,
+            resubmit_errors=args$resubmit_errors))
+} else {
+    pegasus_dir = paste0(reg$file.dir, "/pegasus")
+    if (dir.exists(pegasus_dir)) {
+        unlink(pegasus_dir)
+    }
+    dir.create(pegasus_dir)
+    jobs = getJobTable(findNotDone())
+    if (!args$resubmit_errors) {
+        errs = findErrors()
+        jobs = jobs[!job.id %in% errs$job.id]
+    }
+    dag = vector("character", nrow(jobs))
+    dag_rm = vector("character", nrow(jobs))
+    edges_rm = vector("character", nrow(jobs))
+    for (i in 1:nrow(jobs)) {
+        if (is.na(jobs[i, job.hash])) {
+            jobs[i, job.hash:=digest::digest(jobs[i, ], algo="md5")]
+        }
+        collection = makeJobCollection(jobs[i, job.id])
+        collection_file = paste0(reg$file.dir, "/pegasus/", jobs[i, job.hash],
+                                 ".rds")
+        saveRDS(collection, collection_file)
+        dag[i] = paste0("TASK ", jobs[i, job.hash], " -c 1",
+            " bash -Eeo pipefail -c ",
+            "\"Rscript -e \\\'batchtools::doJobCollection(\\\"", 
+            collection_file, "\\\")\\\'\"")
+        dag_rm[i] = paste0("TASK ", jobs[i, job.hash], "_rm -c 1",
+            "bash -Eeo pipefail -c \"rm ", collection_file, "\"")
+        edges_rm[i] = paste0("EDGE ", jobs[i, job.hash], " ",
+            jobs[i, job.hash], "_rm")
+    }
+    fileConn = file(paste0(reg$file.dir, "/pegasus/tasks.dag"))
+    writeLines(dag, fileConn)
+    close(fileConn)
+}
 
 #mlr3extralearners::create_learner(
     #path="learners",
